7744f127303f59df5e241090a36479652febd395
TMRGZ <miguelrg5605@outlook.es>
2023-05-30 22:25:35 +0200

    FEATURE: Port to SCSS

--- src/app/modules/movie-list/components/movie-cover/movie-cover.component.ts
+++ src/app/modules/movie-list/components/movie-cover/movie-cover.component.ts
@@ -4,7 +4,7 @@ import {Movie} from "../../domain/model/movie";
 @Component({
   selector: 'app-movie-cover',
   templateUrl: './movie-cover.component.html',
-  styleUrls: ['./movie-cover.component.css']
+  styleUrls: ['./movie-cover.component.scss']
 })
 export class MovieCoverComponent {
 
--- src/app/modules/movie-list/components/movie-list/movie-list.component.ts
+++ src/app/modules/movie-list/components/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import {Movie} from "../../domain/model/movie";
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
 
