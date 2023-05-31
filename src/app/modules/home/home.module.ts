1888eaa7a0d1ee5f8dd44fdff9b19f9aaaf285a4
TMRGZ <miguelrg5605@outlook.es>
2023-05-31 23:59:11 +0200

    FIX: Refactored footer, created dedicated components for navbar and footer

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {HomeComponent} from './components/home/home.component';
 import {MovieListModule} from "../movie-list/movie-list.module";
+import {HomeFooterComponent} from './components/home-footer/home-footer.component';
+import {HomeNavbarComponent} from './components/home-navbar/home-navbar.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HomeFooterComponent,
+    HomeNavbarComponent
   ],
   imports: [
     CommonModule,
